2aa60f1df30695499dba8594233e5fec6ee7a02a
Chris <cgoppedal@gmail.com>
2021-04-01 18:44:07 -0400

    Solved 9 more algorithms.

--- tasksType/tasksType.ts
+++ tasksType/tasksType.ts
@@ -1,6 +1,19 @@
-export function tasksTypes(deadlines: number[], day: number): number[] {
+export function tasksTypes(deadlines: number[], today: number): number[] {
 
+    // 1st index is <= date
+    // 2nd index is date + 1 to date + 7
+    // 3rd index is everything else
+    return deadlines.reduce((accumulator, deadline) => {
+        if (deadline <= today) {
+            accumulator[0]++;
+        } else if (deadline <= today + 7) {
+            accumulator[1]++;
+        } else {
+            accumulator[2]++;
+        }
+        return accumulator;
+    }, [0,0,0]);
 }
 
-// console.log(tasksTypes([1, 2, 3, 4, 5], 2));
-// console.log(tasksTypes([1, 2, 4, 2, 10, 3, 1, 4, 5, 4, 9, 8], 1));
+console.log(tasksTypes([1, 2, 3, 4, 5], 2));
+console.log(tasksTypes([1, 2, 4, 2, 10, 3, 1, 4, 5, 4, 9, 8], 1));
--- uniqueDigitProducts/uniqueDigitsProducts.ts
+++ uniqueDigitProducts/uniqueDigitsProducts.ts
@@ -1,4 +1,13 @@
-export function uniqueDigitProducts(a: number[]): number {
+export function uniqueDigitProducts(productsArr: number[]): number {
+    let uniqueMap = productsArr.reduce((accumulator, arr) => {
+        let total = 1;
+        arr.toString().split('').forEach(digit => {
+            total = total * +digit;
+        });
+        accumulator.set(total, null);
+        return accumulator;
+    }, new Map())
+    return uniqueMap.size;
 }
 
-// console.log(uniqueDigitProducts([2, 8, 121, 42, 222, 23]));
+console.log(uniqueDigitProducts([2, 8, 121, 42, 222, 23]));
